2c66fee7f6c50717972ce5cb0978d5725e76dc70
hollyrock <thoroiwa@gmail.com>
2015-03-25 18:23:37 +0900

    Modified some comments# Please enter the commit message for your changes. Lines starting# with '#' will be ignored, and an empty message aborts the commit.# On branch master# Changes to be committed:#   (use "git reset HEAD <file>..." to unstage)##	modified:   app.js#

--- app.js
+++ app.js
@@ -6,7 +6,83 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 //
-// Arduino Control (add)
+/// MongoDB
+//
+
+// Preparing for MongoDB access module
+// Make connection to sensedevice db
+var mongo =require('mongoskin');
+var db = mongo.db("mongodb://localhost:27017/sensedevice", {native_parser:true});
+
+
+//
+/// Web Server setup
+//
+
+// route setup
+var routes = require('./routes/index');
+var users = require('./routes/users');
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// Uncomment after placng your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Make our db accessible to our router
+// This code enable to return DB query by http://localhost:port/users/capdata
+app.use(function(req, res, next){
+    req.db = db;
+    next();
+});
+
+app.use('/', routes);
+app.use('/users', users);
+
+/// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+
+//
+/// error handlers
+//
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+
+//
+/// Connect Raspberry Pi to Arduino with XBee (API)
 //
 var SerialPort = require('serialport').SerialPort;
 var xbee_api = require("xbee-api");
@@ -59,75 +135,8 @@ serialport.on('data', function(input) {
             console.log('Something wrong on received Data..');
             return;
         }
-     //io.sockets.json.emit('message', { value: jData});
 
 });
-
-// Preparing for MongoDB access module
-// Make connection to sensedevice db
-var mongo =require('mongoskin');
-var db = mongo.db("mongodb://localhost:27017/sensedevice", {native_parser:true});
-
-// route setup
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
-var app = express();
-
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// Uncomment after placng your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Make our db accessible to our router
-// This code enable to return DB query by http://localhost:port/users/capdata
-app.use(function(req, res, next){
-    req.db = db;
-    next();
-});
-
-app.use('/', routes);
-app.use('/users', users);
-
-/// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-/// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
-
 
 
 module.exports = app;
